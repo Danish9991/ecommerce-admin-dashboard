9560fd963d006afc85a8c337718df0b3e8340241
macbook <danishmir9991@gmail.com>
2023-07-29 23:13:16 +0530

    updated

---
+++ app/api/stores/route.ts
@@ -0,0 +1,38 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name } = body;
+    console.log('mnamma', name);
+    
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+
+    console.log('store', store);
+    
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[store_post]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,15 +1,17 @@
 "use client"
 
-import React from 'react'
+import React, { useState } from 'react'
 import * as z from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
 
 import { Modal } from '@/components/ui/modal'
 import { useStoreModal } from '@/hooks/use-store-modal'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { toast } from 'react-hot-toast'
 
 
 const formSchema = z.object({
@@ -19,6 +21,8 @@ const formSchema = z.object({
 const StoreModal = () => {
     const storeModal = useStoreModal();
 
+    const [loading, setLoading] = useState(false)
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver : zodResolver(formSchema),
         defaultValues : {
@@ -27,7 +31,16 @@ const StoreModal = () => {
     })
 
     const onSubmit = async( values : z.infer<typeof formSchema>) => {
-        console.log(values);  
+        console.log(values); 
+        try {
+          setLoading(true);
+          const response = await axios.post('/api/stores', values)
+          toast.success("store created.")
+        } catch (error) {
+          toast.error("something went wrong.")
+        } finally {
+          setLoading(false)
+        }
     }
   return (
     <Modal
@@ -46,7 +59,7 @@ const StoreModal = () => {
                 <FormItem>
                     <FormLabel>Name</FormLabel>
                     <FormControl>
-                        <Input placeholder='E-commerce' {...field}/>
+                        <Input disabled={loading} placeholder='E-commerce' {...field}/>
                     </FormControl>
                     <FormMessage />
                 </FormItem>
@@ -54,8 +67,8 @@ const StoreModal = () => {
             />
 
             <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
-              <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-              <Button type='submit'>Continue</Button>
+              <Button variant="outline" disabled={loading} onClick={storeModal.onClose}>Cancel</Button>
+              <Button type='submit' disabled={loading}>Continue</Button>
             </div>
 
           </form>
